66f37d8bd80f9e1e78ede84e66df46a65fa07a33
Ryan White <401300+beachwood23@users.noreply.github.com>
2024-06-12 15:40:21 -0400

    Updating qotd format.

--- javascripts/quotd.js
+++ javascripts/quotd.js
@@ -1,4 +1,2 @@
-document.writeln('<b><font size=\'-1\'><a target=\'_blank\' class=\'tqpHeader\' href=\"http://www.quotationspage.com/qotd.html\">Quote of the Day (June 12, 2024)</a></font></b><br>');
-document.writeln('<dl>');
-document.writeln('<dt class=\'tqpQuote\'>He who asks is a fool for five minutes, but he who does not ask remains a fool forever.</dt>');
-document.writeln('</dl>');
+document.writeln('<font size=\'-1\'><a target=\'_blank\' class=\'tqpHeader\' href=\"http://www.quotationspage.com/qotd.html\">Quote of the Day (June 12, 2024)</a></font><br>');
+document.writeln('<blockquote class=\'tqpQuote\'>He who asks is a fool for five minutes, but he who does not ask remains a fool forever.</dt>');
