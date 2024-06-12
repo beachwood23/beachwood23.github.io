2e6ff90c1231387312756f898766082915ae76c5
Ryan White <401300+beachwood23@users.noreply.github.com>
2024-06-12 10:33:55 -0400

    Updating js for quote of the day to be local.

---
+++ javascripts/quotd.js
@@ -0,0 +1,5 @@
+document.writeln('<b><font size=\'-1\'><a target=\'_blank\' class=\'tqpHeader\' href=\"http://www.quotationspage.com/qotd.html\">Quote of the Day (June 12, 2024)</a></font></b><br>');
+document.writeln('<dl>');
+document.writeln('<dt class=\'tqpQuote\'>He who asks is a fool for five minutes, but he who does not ask remains a fool forever.</dt>');
+document.writeln('<dd> <b><font size=\'-1\'>~ Chinese Proverb</font></b></dd><p> </p>');
+document.writeln('</dl>');
