b0c3a6c830af69ab5add7264efaecc023a478b5a
Ryan White <401300+beachwood23@users.noreply.github.com>
2024-06-13 08:44:50 -0400

    Removing custom js, customizing quotd after it loads.

--- javascripts/quotd.js
+++
@@ -1,2 +0,0 @@
-document.writeln('<font size=\'-1\'><a target=\'_blank\' class=\'tqpHeader\' href=\"http://www.quotationspage.com/qotd.html\">Quote of the Day (June 12, 2024)</a></font><br>');
-document.writeln('<blockquote class=\'tqpQuote\'>He who asks is a fool for five minutes, but he who does not ask remains a fool forever.</dt>');
